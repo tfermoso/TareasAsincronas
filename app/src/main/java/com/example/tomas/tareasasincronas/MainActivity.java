c0e0febb92dbf7095c2a4564f42a737e1760090f
tfermoso <tomas.fermoso@gmail.com>
2017-11-23 19:54:17 +0100

    Http- lector de noticias

--- app/src/main/java/com/example/tomas/tareasasincronas/MainActivity.java
+++ app/src/main/java/com/example/tomas/tareasasincronas/MainActivity.java
@@ -13,14 +13,26 @@ import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button btn1;
     Button btn2;
     Button btn3;
     Button btn4;
+    Button btn5;
     ProgressBar progressBar;
+    String urlconexion="http://ep00.epimg.net/rss/elpais/portada.xml";
+    TextView noticias;
+    Conexion conexion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +42,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn2=(Button) findViewById(R.id.btnHilo);
         btn3=(Button) findViewById(R.id.btnAsinkTask);
         btn4=(Button) findViewById(R.id.btnNotificacion);
+        btn5=(Button) findViewById(R.id.btnHttp);
+        noticias = (TextView) findViewById(R.id.txtNoticias);
 
         btn1.setOnClickListener(this);
         btn2.setOnClickListener(this);
         btn3.setOnClickListener(this);
         btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+
         progressBar=(ProgressBar) findViewById(R.id.progressBar);
     }
 
@@ -71,10 +87,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 //Enviamos la notificación
                 NotificationManager notificationManager=(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
                 notificationManager.notify(0,builder.build());
-
-
-
-
+                break;
+            case R.id.btnHttp:
+                noticias.setText("");
+                conexion=new Conexion();
+                conexion.execute();
                 break;
             default:
                 break;
@@ -152,6 +169,69 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     }
+
+    public class Conexion extends AsyncTask<Void,Integer,String>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+        @Override
+        protected String doInBackground(Void... params) {
+            String salida="";
+            int i=0,j=0;
+            try {
+                URL url=new URL(urlconexion);//url de donde obtenemos la info
+                //Abrimos la conexión
+                HttpURLConnection connection=(HttpURLConnection) url.openConnection();
+                connection.setRequestProperty("User-Agent","Mozilla/5.0" +
+                        " (Linux;Android 1.5; es-ES) Ejemplo Http");
+
+                int respuesta = connection.getResponseCode();
+                if(respuesta==HttpURLConnection.HTTP_OK){
+                    BufferedReader lector=new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String linea = lector.readLine();
+                    while(linea!=null){
+                        if(linea.indexOf("<title><![CDATA[")>=0){
+                            i=linea.indexOf("<title>")+16;
+                            j=linea.indexOf("</title>")-3;
+                            salida +=linea.substring(i,j);
+                            salida +="\n-----------\n";
+                        }
+                        linea=lector.readLine();
+                    }
+                    lector.close();
+                }else{
+                    salida="Página no encontrada";
+                }
+                connection.disconnect();
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return salida;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            //super.onPostExecute(s);
+            noticias.setText(s);
+
+
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
+
+
+    }
+
+
 
 }
 
